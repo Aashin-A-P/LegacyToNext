91eee93d7c11cb3084395ff04c65eac1dfb50dd2
ashika <nashika231@gmail.com>
2025-12-14 22:18:31 +0530

    weird not weird number ✔

---
+++ Ashika/HackerRank/weird_and_notweird_number.py
@@ -0,0 +1,14 @@
+n=int(input())
+if n<101 and n>-1:
+    if n%2==1:
+        print('Weird')
+    
+    elif n%2==0 and n>1 and n<6:
+        print("Not Weird")
+    
+    elif n%2==0 and n>5 and n<21:
+        print("Weird")
+    
+    else:
+        print("Not Weird") 
+      
