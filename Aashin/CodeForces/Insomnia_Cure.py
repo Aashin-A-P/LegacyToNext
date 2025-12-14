d54b6b64b7e5e9c1f18db04fa2957a17f2e34616
Aashin A P <apaashin@gmail.com>
2025-12-15 00:19:49 +0530

    CodeForces - Insomnia cure

---
+++ Aashin/CodeForces/Insomnia_Cure.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+co = 0
+for i in range(1,e+1):
+    if i%a == 0 or i%b == 0 or i%c ==0 or i%d==0:
+        co+=1
+print(co)
