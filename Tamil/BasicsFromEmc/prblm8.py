748656a7fbc1116370e8340ab173c668074b3118
tamilvani-m <tamilkamala2529@gmail.com>
2025-12-16 18:12:47 +0530

    ifelse true means ask the user input

---
+++ Tamil/BasicsFromEmc/prblm8.py
@@ -0,0 +1,8 @@
+score=int(input("enter percentage:"))
+if(score>70):
+    name=input("enter name:")
+    department=input("enter dept:")
+    location=input("enter location:")
+    print("you are eligible")
+else:
+    print("you are not eligible")
