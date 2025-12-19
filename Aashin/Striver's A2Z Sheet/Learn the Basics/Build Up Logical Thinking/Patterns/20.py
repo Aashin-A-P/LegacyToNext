9dacb8accd6b39b9131cc26b7e7206d04b32eb62
Aashin A P <apaashin@gmail.com>
2025-12-19 22:33:53 +0530

    Strivers Sheet - Pattern 20

---
+++ Aashin/Striver's A2Z Sheet/Learn the Basics/Build Up Logical Thinking/Patterns/20.py
@@ -0,0 +1,20 @@
+s = 8
+for i in range(1,6):
+    for j in range(i):
+        print('*', end='')
+    for j in range(s):
+        print(' ', end = '')
+    for j in range(i):
+        print('*', end='')
+    print()
+    s-=2
+s = 2
+for i in range(4,0,-1):
+    for j in range(i):
+        print('*', end='')
+    for j in range(s):
+        print(' ', end = '')
+    for j in range(i):
+        print('*', end='')
+    print()
+    s+=2
