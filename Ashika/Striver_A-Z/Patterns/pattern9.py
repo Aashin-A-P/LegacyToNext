0b0125080497d946d0098bddf9423c82d39f95a3
ashika <nashika231@gmail.com>
2025-12-21 18:52:05 +0530

    Diamond shape ✔

---
+++ Ashika/Striver_A-Z/Patterns/pattern9.py
@@ -0,0 +1,12 @@
+n=int(input())
+a=(n-1)//2
+for i in range(1,n+1,2):
+    space=' '*a
+    print(space+'*'*i)
+    a-=1
+
+b=0    
+for i in range(n,0,-2):
+    space=' '*b
+    print(space+'*'*i)
+    b+=1
