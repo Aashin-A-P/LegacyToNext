fbcbe83a53557ade3ad2cb0c4beb1a668bd24c17
ashika <nashika231@gmail.com>
2025-12-29 18:15:06 +0530

    Find max  ✔

---
+++ Ashika/Striver_A-Z/Sorting/bubble.py
@@ -0,0 +1,14 @@
+A=[64,24,12,11,22]
+size=len(A)
+for i in range(size):
+    key=A[i]
+    j=0
+    while(j<size-1):
+        if(A[j]>A[j+1]):
+            c=A[j]
+            A[j]=A[j+1]
+            A[j+1]=c
+        j+=1
+    size-=1
+for i in A:
+    print(i)
