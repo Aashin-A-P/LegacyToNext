8a1e531a409059ba37d3288d7876a544329f108c
ashika <nashika231@gmail.com>
2025-12-30 17:30:04 +0530

    Modified insertion ✔

--- Ashika/Striver_A-Z/Sorting/insertion.py
+++ Ashika/Striver_A-Z/Sorting/insertion.py
@@ -2,13 +2,12 @@ A=[65,24,12,22,11]
 for i in range(len(A)):
     key=A[i]
     j=i-1
-    k=i
-    while(j>=0 and key<A[j]):
+    while(j>=0 and A[j]>key):
         A[j+1]=A[j]
         j-=1
-    if(i!=j):
-        j+=1
+    j+=1
+    if(key!=A[j]):
         A[j]=key
-
+        
 for i in A:
     print(i)
